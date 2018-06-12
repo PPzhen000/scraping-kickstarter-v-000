d1d46f0bbc7e65ba6407695ea4e399ea8c0ad972
Ariel Zhen <zhenpingping@live.com>
2018-06-12 04:58:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
-# require libraries/modules here
+require 'Nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/Kickstarter.html') 
+
+  Kickstarter = Nokogiri::HTML(html) 
 end
